a51abf21cbe03f50174cf3e238d03ea60ba6a848
MichaelMWW <xiaofeng200485@163.com>
2016-11-06 21:27:47 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+
+namespace ConsoleHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Int32 i=5,j = 15;
+            Int32 k = i + j;
+
+            
+            Console.ReadLine();
+            XmlTextWriter xtw = new XmlTextWriter(@"d:\test.txt", System.Text.Encoding.ASCII);
+        }
+    }
+}
